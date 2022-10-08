3d9e06eccfd41fa80c145ef0aa4b84eaeabaf5f2
Veronika Kravchenko <aniregma@gmail.com>
2022-10-08 22:08:31 +0300

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,27 @@
-import Profile from "./User";
+import { Profile } from './Profile/Profile';
 import user from '../user.json';
+//  
 export const App = () => {
   return (
     <div>
       <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
     </div>
   );
 };
 
+{/* <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/> */}
 // export const App = () => {
 //   return (
 //     <div
--- src/index.js
+++ src/index.js
@@ -3,24 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-// import user from '../user.json';
-
-// приклад використання:
-/* <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/> */
- 
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>, document.querySelector('root')
+  </React.StrictMode>
 );
+// , document.querySelector('root')
 
 
