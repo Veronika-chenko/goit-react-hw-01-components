acd0321fd70cbbba285cc9d9631ccafe65c4ca11
Veronika Kravchenko <aniregma@gmail.com>
2022-10-08 22:12:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,13 +15,6 @@ export const App = () => {
   );
 };
 
-{/* <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/> */}
 // export const App = () => {
 //   return (
 //     <div
